9d938f49929402e179037e38dd30b188de74a876
Alfarell Muchamad Yuwanto <malfarell@gmail.com>
2024-10-02 08:47:28 +0700

    init submission project

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const Hapi = require("@hapi/hapi");
+
+const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
+const port = process.env.PORT || 3000;
+
+const init = async () => {
+  const server = Hapi.server({
+    host,
+    port,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  server.route();
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+};
+
+init();
